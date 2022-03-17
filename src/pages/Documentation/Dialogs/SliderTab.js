a5667005b8c696d346dd5981a6eae166bc0c380d
philippe soubrier <philippe.soubrier@gmail.com>
2022-03-17 16:35:25 +0100

    first sliders

---
+++ src/pages/Documentation/Dialogs/SliderTab.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { TextField, Stack, Button } from '@mui/material';
+import SpanSlider from '../../../components/Sliders/SpanSlider';
+import IntervalSlider from '../../../components/Sliders/IntervalSlider';
+
+
+export default function SliderTab(props) {
+  const { title, description } = props
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleSubmit = () => {
+    console.log(date.value);
+    
+  };
+  
+ 
+  return (
+    <Stack component="form"  spacing={1} sx={{ width: '100%' }}>
+      <Typography component={"span"} variant="h5">{title}</Typography>
+      <Typography>Date</Typography>
+        <TextField
+          id="date"
+          type="date"
+          defaultValue="2022-03-17"
+          sx={{ width: 220 }}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      <Typography>Time span</Typography>
+        <SpanSlider/>
+      <Typography>Location</Typography>
+      <Box sx={{ '& .MuiTextField-root': { m: 1} }}>
+        <TextField
+          label="Latitude"
+          id="latitude"
+          defaultValue="50.850346"
+        />
+        <TextField
+          label="Longitude"
+          id="longitude"
+          defaultValue="4.351721"
+        />        
+      </Box>
+      <Typography>Shadows interval</Typography>
+      <IntervalSlider/>
+      <Button 
+        onClick={handleSubmit}
+        variant="outlined" 
+        color="success"
+      >
+        Submit
+      </Button>
+    </Stack>
+  );
+}
