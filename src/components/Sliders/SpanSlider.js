e19fe84a31aecedb95da9c20e92d3710a8674ea4
philippe soubrier <philippe.soubrier@gmail.com>
2022-03-19 14:05:32 +0100

    working variables, not theming

--- src/components/Sliders/SpanSlider.js
+++ src/components/Sliders/SpanSlider.js
@@ -1,59 +1,50 @@
+import Slider, { SliderThumb } from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
 import React, { useState } from "react";
-import Slider, { SliderThumb } from '@mui/material/Slider';
-import { styled } from '@mui/material/styles';
 
-
-export default function SpanSlider(props) {
+export default function SpanSlider() {
   const Span = styled(Slider)(({ theme }) => ({
-    color: '#52af77',
+    color: "#52af77",
     height: 8,
-    '& .MuiSlider-track': {
-      border: 'none',
+    "& .MuiSlider-track": {
+      border: "none",
     },
-    '& .MuiSlider-thumb': {
+    "& .MuiSlider-thumb": {
       height: 24,
       width: 24,
-      backgroundColor: '#fff',
-      border: '2px solid currentColor',
-      '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-        boxShadow: 'inherit',
+      backgroundColor: "#fff",
+      border: "2px solid currentColor",
+      "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+        boxShadow: "inherit",
       },
-      '&:before': {
-        display: 'none',
+      "&:before": {
+        display: "none",
       },
-      '& .airbnb-bar': {
+      "& .airbnb-bar": {
         height: 9,
         width: 1,
-        backgroundColor: 'currentColor',
+        backgroundColor: "currentColor",
         marginLeft: 1,
         marginRight: 1,
       },
     },
-    '& .MuiSlider-track': {
-      height: 3,
-    },
-    '& .MuiSlider-rail': {
-      color: theme.palette.mode === 'dark' ? '#bfbfbf' : '#d8d8d8',
-      opacity: theme.palette.mode === 'dark' ? undefined : 1,
-      height: 3,
-    },
-    '& .MuiSlider-valueLabel': {
+    "& .MuiSlider-valueLabel": {
       lineHeight: 1.2,
       fontSize: 12,
-      background: 'unset',
+      background: "unset",
       padding: 0,
       width: 32,
       height: 32,
-      borderRadius: '50% 50% 50% 0',
-      backgroundColor: '#52af77',
-      transformOrigin: 'bottom left',
-      transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-      '&:before': { display: 'none' },
-      '&.MuiSlider-valueLabelOpen': {
-        transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+      borderRadius: "50% 50% 50% 0",
+      backgroundColor: "#52af77",
+      transformOrigin: "bottom left",
+      transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
+      "&:before": { display: "none" },
+      "&.MuiSlider-valueLabelOpen": {
+        transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
       },
-      '& > *': {
-        transform: 'rotate(45deg)',
+      "& > *": {
+        transform: "rotate(45deg)",
       },
     },
   }));
@@ -73,31 +64,37 @@ export default function SpanSlider(props) {
   const marks = [
     {
       value: 7,
-      label: '7h',
+      label: "7h",
     },
     {
       value: 12,
-      label: '12h',
+      label: "12h",
     },
     {
       value: 17,
-      label: '17h',
+      label: "17h",
     },
     {
       value: 22,
-      label: '22h',
+      label: "22h",
     },
   ];
 
+  const [val, setVal] = useState([6, 16]);
+  const updateRange = (e, data) => {
+    setVal(data);
+  };
+
   return (
     <Span
-        valueLabelDisplay="auto"
-        components={{ Thumb: SpanComponent }}
-        defaultValue={[7, 17]}
-        step={1}
-        marks={marks}
-        min={5}
-        max={24}
+      valueLabelDisplay="auto"
+      components={{ Thumb: SpanComponent }}
+      defaultValue={[6, 16]}
+      onChange={updateRange}
+      step={1}
+      marks={marks}
+      min={5}
+      max={24}
     />
   );
 }
--- src/pages/Documentation/Dialogs/SliderTab.js
+++ src/pages/Documentation/Dialogs/SliderTab.js
@@ -1,63 +1,185 @@
+import { Button, Slider, SliderThumb, Stack, TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import { createTheme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 import React, { useState } from "react";
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { TextField, Stack, Button } from '@mui/material';
-import SpanSlider from '../../../components/Sliders/SpanSlider';
-import IntervalSlider from '../../../components/Sliders/IntervalSlider';
+import { styled } from '@mui/material/styles';
 
+const theme1 = createTheme({
+  root: {
+    fontSize: "1rem",
+    color: "#52af77",
+    height: 8,
+    "& .MuiSlider-track": {
+      border: "none",
+    },
+    "& .MuiSlider-thumb": {
+      height: 24,
+      width: 24,
+      backgroundColor: "#fff",
+      border: "2px solid currentColor",
+      "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+        boxShadow: "inherit",
+      },
+      "&:before": {
+        display: "none",
+      },
+      "& .airbnb-bar": {
+        height: 9,
+        width: 1,
+        backgroundColor: "currentColor",
+        marginLeft: 1,
+        marginRight: 1,
+      },
+    },
+    "& .MuiSlider-valueLabel": {
+      lineHeight: 1.2,
+      fontSize: 12,
+      background: "unset",
+      padding: 0,
+      width: 32,
+      height: 32,
+      borderRadius: "50% 50% 50% 0",
+      backgroundColor: "#52af77",
+      transformOrigin: "bottom left",
+      transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
+      "&:before": { display: "none" },
+      "&.MuiSlider-valueLabelOpen": {
+        transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
+      },
+      "& > *": {
+        transform: "rotate(45deg)",
+      },
+    },
+  },
+});
+
+const marks1 = [
+  {
+    value: 7,
+    label: "7h",
+  },
+  {
+    value: 12,
+    label: "12h",
+  },
+  {
+    value: 17,
+    label: "17h",
+  },
+  {
+    value: 22,
+    label: "22h",
+  },
+];
+const marks2 = [
+  {
+    value: 0.5,
+    label: "30min",
+  },
+  {
+    value: 1,
+    label: "1h",
+  },
+  {
+    value: 2,
+    label: "2h",
+  },
+  {
+    value: 3,
+    label: "3h",
+  },
+];
+
+function SpanComponent(props) {
+  const { children, ...other } = props;
+  return (
+    <SliderThumb {...other}>
+      {children}
+      <span className="airbnb-bar" />
+      <span className="airbnb-bar" />
+      <span className="airbnb-bar" />
+    </SliderThumb>
+  );
+}
 
 export default function SliderTab(props) {
-  const { title, description } = props
-  const [value, setValue] = React.useState(0);
+  const { title } = props;
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const [date, setDate] = useState("2022-03-17");
+  const [range, setRange] = useState([7, 17]);
+  const [interval, setInterval] = useState(1);
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  const updateDate = (e, data) => {
+    setDate(data);
   };
-  const [tijdspan, setTijdspan] = useState('');
-  const toSliderTab = (data) => {
-    setTijdspan(data);
+  const updateRange = (e, data) => {
+    setRange(data);
+  };
+  const updateInterval = (e, data) => {
+    setInterval(data);
+  };
+  const updateLatitude = (e, data) => {
+    setLatitude(data);
+  };
+  const updateLongitude = (e, data) => {
+    setLongitude(data);
   };
 
-  const handleSubmit = () => {
-    console.log(date.value);
-    console.log(tijdspan);
-  };
-   
   return (
-    <Stack component="form"  spacing={1} sx={{ width: '100%' }}>
-      <Typography component={"span"} variant="h5">{title}</Typography>
+    <Stack component="form" spacing={1} sx={{ width: "100%" }}>
+      <Typography component={"span"} variant="h5">
+        {title}
+      </Typography>
       <Typography>Date</Typography>
-        <TextField
-          id="date"
-          type="date"
-          defaultValue="2022-03-17"
-          sx={{ width: 220 }}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
+      <TextField
+        type="date"
+        value={date}
+        onChange={updateDate}
+        sx={{ width: 220 }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
       <Typography>Time span</Typography>
-        <SpanSlider toSliderTab={toSliderTab}/>
+      <Slider
+        theme={theme1}
+        valueLabelDisplay="auto"
+        value={range}
+        onChange={updateRange}
+        components={{ Thumb: SpanComponent }}
+        step={1}
+        marks={marks1}
+        min={5}
+        max={24}
+      />
+
       <Typography>Location</Typography>
-      <Box sx={{ '& .MuiTextField-root': { m: 1} }}>
+      <Box sx={{ "& .MuiTextField-root": { m: 1 } }}>
         <TextField
           label="Latitude"
-          id="latitude"
-          defaultValue="50.850346"
+          defaultValue={50.850346}
+          onChange={updateLatitude}
         />
         <TextField
           label="Longitude"
-          id="longitude"
-          defaultValue="4.351721"
-        />        
+          defaultValue={4.351721}
+          onChange={updateLongitude}
+        />
       </Box>
       <Typography>Shadows interval</Typography>
-      <IntervalSlider/>
-      <Button 
-        onClick={handleSubmit}
-        variant="outlined" 
-        color="success"
-      >
+      <Slider
+        valueLabelDisplay="auto"
+        value={interval}
+        onChange={updateInterval}
+        step={0.25}
+        marks={marks2}
+        min={0.25}
+        max={4}
+      />
+      <Button variant="outlined" color="success">
         Submit
       </Button>
     </Stack>
