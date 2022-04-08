30a94434acb433e147c614fa12db78fc3cc9a783
Lieselotte Wiels <lieselotte.wiels@gmail.com>
2022-04-08 16:05:14 +0200

    scrollen en nieuwe kader voor de map

--- src/pages/DemoPage/index.js
+++ src/pages/DemoPage/index.js
@@ -1,26 +1,35 @@
-import React from 'react'
-import { Grid } from '@mui/material'
-import GetProjects from '../Documentation/Dialogs/GetProjects'
-import CreateProject from '../Documentation/Dialogs/CreateProject'
-import CreateDataset from '../Documentation/Dialogs/CreateDataset'
-import GetAllDatasets from '../Documentation/Dialogs/GetAllDatasets'
-import AlignDistributions from '../Documentation/Dialogs/AlignDistributions'
-import BasicTabs from '../Documentation/Dialogs/BasicTabs'
-import SliderTab from '../Documentation/Dialogs/SliderTab'
-import Viewer from '../../components/Viewer/3dviewer'
+import React from "react";
+import { Grid } from "@mui/material";
+import GetProjects from "../Documentation/Dialogs/GetProjects";
+import CreateProject from "../Documentation/Dialogs/CreateProject";
+import CreateDataset from "../Documentation/Dialogs/CreateDataset";
+import GetAllDatasets from "../Documentation/Dialogs/GetAllDatasets";
+import AlignDistributions from "../Documentation/Dialogs/AlignDistributions";
+import BasicTabs from "../Documentation/Dialogs/BasicTabs";
+import SliderTab from "../Documentation/Dialogs/SliderTab";
+import Viewer from "../../components/Viewer/3dviewer";
+import "./index_demo.css";
 
 const DemoPage = () => {
-    return (
-        <Grid style={{ textAlign: "justify" }} container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-            <Grid item xs={0} sm={2} md={3} />
-            <Grid item style={{ margin: 15 }} xs={4} sm={4} md={6}>
-            <div style={subComponentStyle}>
-                    <SliderTab title={"8. Our sliders."}/>
-                </div>
-                <div style={subComponentStyle}>
-                    <Viewer title={"7. Our viewer."}/>
-                </div>
-                {/* <div style={subComponentStyle}>
+  return (
+    <Grid
+      style={{ textAlign: "justify" }}
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 4, sm: 8, md: 12 }}
+    >
+      <Grid item xs={0} sm={2} md={3}/>
+      <Grid item style={{ margin: 15 }} xs={4} sm={4} md={6}>
+        <div style={subComponentStyle}>
+          <SliderTab title={"1. Our sliders."} />
+        </div>
+        <div style={subComponentStyle}>
+          <Viewer title={"2. Mapping."} />
+        </div>
+        <div style={subComponentStyle}>
+          <Viewer title={"3. Our viewer."} />
+        </div>
+        {/* <div style={subComponentStyle}>
                     <CreateProject title={"1. (optional) create a project (login required)"} />
                 </div>
                 <div style={subComponentStyle}>
@@ -38,12 +47,17 @@ const DemoPage = () => {
                 <div style={subComponentStyle}>
                     <BasicTabs title={"6. Scroll through the enrichment modules, visualise and query the project."}/>
                 </div> */}
-            </Grid>
-            <Grid item xs={0} sm={2} md={3} />
-        </Grid>
-    )
-}
+      </Grid>
+      <Grid item xs={0} sm={2} md={3} />
+    </Grid>
+  );
+};
 
-const subComponentStyle = { marginTop: 30, border: "2px solid gray", borderRadius: 15, padding: 15}
+const subComponentStyle = {
+  marginTop: 30,
+  border: "2px solid gray",
+  borderRadius: 15,
+  padding: 15,
+};
 
-export default DemoPage
+export default DemoPage;
