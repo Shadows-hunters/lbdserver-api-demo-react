013be05852dd7e7c7afb6a0d44eab0e267eb22d9
Lieselotte Wiels <lieselotte.wiels@gmail.com>
2022-04-21 12:56:47 +0200

    mapview erin

--- src/components/map/MapTab.js
+++ src/components/map/MapTab.js
@@ -1,15 +1,46 @@
-import { Stack } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import "ol/ol.css";
+import { Typography } from "@mui/material";
 
-export default function MapTab(props) {
+function MapTab(props) {
   const { title } = props;
 
+  const [map, setMap] = useState();
+  const mapElement = useRef();
+  const mapRef = useRef();
+  mapRef.current = map;
+
+  useEffect(() => {
+    const initialMap = new Map({
+      target: mapElement.current,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+    setMap(initialMap);
+  }, []);
+
   return (
-    <Stack spacing={1} sx={{ width: "100%" }}>
+    <div>
       <Typography component={"span"} variant="h5">
         {title}
       </Typography>
-    </Stack>
+      <div
+        style={{ height: "50vh", width: "100%" }}
+        ref={mapElement}
+        className="map-container"
+      />
+    </div>
   );
 }
+
+export default MapTab;
