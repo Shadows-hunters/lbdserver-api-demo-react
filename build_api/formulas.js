3f1ac3508c75e3707acee51c3a4caf019be3c355
philippe <philippe.soubrier@gmail.com>
2022-05-25 08:21:31 +0200

    build api updated

--- build_api/formulas.js
+++ build_api/formulas.js
@@ -4,24 +4,24 @@ var dayjs = require("dayjs");
 //import dayjs from 'dayjs' // ES 2015
 dayjs().format();
 
-function sunPositions(date, start, end, latitude, longitude, interval ) {
-    let i = start;
-    var arr = [];
-    var day = dayjs(date);
+function sunPositions(date, start, end, latitude, offset, interval) {
+  let i = start;
+  var arr = [];
+  var day = dayjs(date);
 
-    while (i <= end) {
-        const d = day.add(i * 60, "minute");
-        let position = SunCalc.getPosition(d, latitude, longitude);
-        let vector = [
-        Math.sin(position.azimuth) * Math.cos(position.altitude),
-        Math.cos(position.azimuth) * Math.cos(position.altitude),
-        Math.sin(position.altitude),
-        ];
-        arr.push(vector);
-        i += interval;
-    }
+  while (i <= end) {
+    const d = day.add(i * 60, "minute");
+    let position = SunCalc.getPosition(d, latitude, 0);
+    let vector = [
+      0,
+      position.azimuth + (offset * Math.PI) / 180.0 + Math.PI/2,
+      position.altitude
+    ];
+    arr.push(vector);
+    i += interval;
+  }
 
-    return JSON.stringify(arr);
+  return arr;
 }
 
 module.exports = { sunPositions };
--- build_api/server.js
+++ build_api/server.js
@@ -1,42 +1,104 @@
-const formulas = require("./formulas");
+const formulas = require("./src/components/calculations/formulas");
 
+var fs = require("fs");
+const cors = require("cors");
 const express = require("express");
 const app = express();
-const PORT = 8090;
+app.use(cors());
+const PORT = 8080;
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.get("/positions", (req, res) => {
-    const { date } = req.body
-    const { start } = req.body
-    const { end } = req.body
-    const { latitude } = req.body
-    const { longitude } = req.body
-    const { interval } = req.body
+app.post("/positions", (req, res) => {
+  const { date, start, end, latitude, offset, interval } = req.body;
 
-    if (!date) {
-        res.status(418).send({ message: 'date needed!'})
-    }
-    if (!start) {
-        res.status(418).send({ message: 'start needed!'})
-    }
-    if (!end) {
-        res.status(418).send({ message: 'end needed!'})
-    }
-    if (!latitude) {
-        res.status(418).send({ message: 'latitude needed!'})
-    }
-    if (!longitude) {
-        res.status(418).send({ message: 'longitude needed!'})
-    }
-    if (!interval) {
-        res.status(418).send({ message: 'interval needed!'})
-    }
+  if (!date) {
+    res.status(418).send({ message: "date needed!" });
+  }
+  if (!start) {
+    res.status(418).send({ message: "start needed!" });
+  }
+  if (!end) {
+    res.status(418).send({ message: "end needed!" });
+  }
+  if (!latitude) {
+    res.status(418).send({ message: "latitude needed!" });
+  }
+  if (!offset) {
+    res.status(418).send({ message: "longitude needed!" });
+  }
+  if (!interval) {
+    res.status(418).send({ message: "interval needed!" });
+  }
 
-    res.status(200).send(
-        formulas.sunPositions(date, start, end, latitude, longitude, interval)
-    );   
+  res
+    .status(200)
+    .send(formulas.sunPositions(date, start, end, latitude, offset, interval));
+});
 
+app.post("/map", (req, res) => {
+  const { city, latitude, longitude, forced } = req.body;
+
+  if (!city) {
+    res.status(418).send({ message: "city needed!" });
+  }
+  if (!latitude) {
+    res.status(418).send({ message: "latitude needed!" });
+  }
+  if (!longitude) {
+    res.status(418).send({ message: "longitude needed!" });
+  }
+
+  //   let newData = { [city]: };
+
+  var data = fs.readFileSync("public/map_data/Cleaned BE arr.json");
+  var myObject = JSON.parse(data);
+
+  // check if alrealdy defined
+  if (forced != true) {
+    if (city in myObject) {
+      res
+        .status(423)
+        .send({ message: "city already defined, prop 'forced':true " });
+    }
+  }
+
+  // add to JSON
+  myObject[city] = [latitude, longitude];
+  fs.writeFile(
+    "public/map_data/Cleaned BE arr.json",
+    JSON.stringify(myObject),
+    (err) => {
+      res.status(200).send({ message: "done" });
+    }
+  );
+});
+
+app.get("/map", (req, res) => {
+  const { city } = req.body;
+  if (!city) {
+    res.status(418).send({ message: "city needed!" });
+  }
+
+  var data = fs.readFileSync("public/map_data/Cleaned BE arr.json");
+  var myObject = JSON.parse(data);
+
+  // check if in list
+  if (!(city in myObject)) {
+    res.status(404).send({ message: "city not found" });
+  }
+
+  // send back
+  res.status(200).send(myObject[city]);
+});
+
+
+app.get("/mapAlles", (req, res) => {
+  var data = fs.readFileSync("public/map_data/Cleaned BE arr.json");
+  var myObject = JSON.parse(data);
+
+  // send back
+  res.status(200).send(myObject);
 });
